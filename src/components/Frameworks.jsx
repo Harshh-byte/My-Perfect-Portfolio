056dbd869356bf2f49a0fc9c60336601cfb492fc
Harshh-byte <rockstarharsh7jan@gmail.com>
2025-07-27 20:39:01 +0530

    Project Completed

---
+++ src/components/Frameworks.jsx
@@ -0,0 +1,33 @@
+import { img } from "motion/react-client";
+import { OrbitingCircles } from "./OrbitingCircles";
+
+export function Frameworks() {
+    const skills = [
+        'c',
+        'python',
+        'code',
+        'tailwindcss',
+        'html5',   
+        'css3',
+        'javascript',
+        'react',
+        'vitejs',
+        'mongodb',
+        'git',
+        'nodejs',
+    ];
+    return (
+        <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+            <OrbitingCircles iconSize={40}>
+                {skills.map((skill, index) => (<Icon key={index} src={`assets/logos/${skill}.svg`} />))}
+            </OrbitingCircles>
+            <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
+                {skills.reverse().map((skill, index) => (<Icon key={index} src={`assets/logos/${skill}.svg`} />))}
+            </OrbitingCircles>
+        </div>
+    );
+}
+
+const Icon = ({ src }) => (
+    <img src={src} className="rounded-sm hover:scale-110 duration-200" />
+)
---
+++ src/constants/index.js
@@ -0,0 +1,273 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "E-commerce Platform",
+    description:
+      "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 2,
+        name: ".Net",
+        path: "/assets/logos/dotnet.svg",
+      },
+      {
+        id: 3,
+        name: "Ef Core",
+        path: "/assets/logos/efcore.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Productivity Dashboard",
+    description:
+      "A modern productivity dashboard that delivers real-time insights and tools through a clean, interactive interface.",
+    subDescription: [
+      "Includes an interactive To-Do List, Planner, and Goal Tracker",
+      "Features a fully functional Pomodoro Timer to boost focus.",
+      "Fetches and displays motivational quotes dynamically.",
+      "Built using HTML, CSS, JavaScript, and integrated with third-party APIs.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/dashboard.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "Fonts",
+        path: "/assets/logos/fonts.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "GitHub Profile Finder",
+    description:
+      "A responsive web app to search and explore GitHub user profiles with real-time data integration.",
+    subDescription: [
+      "Built a responsive web application that enables users to search GitHub profiles with real-time data fetching.",
+      "Displays user details including avatar, bio, repository count, follower stats, and account creation date using the GitHub REST API.",
+      "Developed using HTML, TailwindCSS, and JavaScript, with robust error handling and clean, user-friendly UI design.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/profile-finder.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "QuickWeather",
+    description:
+      "A minimalist weather app that provides real-time weather updates based on the user’s current location using the WeatherAPI.",
+    subDescription: [
+      "Fetches real-time weather data using the OpenWeather API and Geolocation API to provide location-based updates.",
+      "Displays temperature, weather conditions, humidity, and wind speed in a responsive UI with light/dark theme toggle.",
+      "Built using HTML, CSS, and JavaScript, with clean design, smooth transitions, and robust error handling.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/weather.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Recipe Book",
+    description:
+      "A modern recipe app to explore, create, and manage your favorite dishes with a clean and responsive interface.",
+    subDescription: [
+      "Browse a collection of built-in recipes with detailed ingredients and instructions.",
+      "Add and manage your own custom recipes, stored using local storage for persistence.",
+      " Developed with React JS and styled using Tailwind CSS for a sleek, mobile-friendly UI.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/recipe-book.png",
+    tags: [
+      {
+        id: 1,
+        name: "React JS",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "GitHub",
+    href: "https://github.com/Harshh-byte",
+    icon: "/assets/socials/github.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/harsh-bhardwaj-b3791b261/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/harshh.hb/",
+    icon: "/assets/socials/instagram.svg",
+  },
+  {
+    name: "X",
+    href: "https://x.com/Harshh_hb",
+    icon: "/assets/socials/x.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software Developer",
+    job: "Security & Defense Projects",
+    date: "2021-2023",
+    contents: [
+      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
+      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
+      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
+      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+    ],
+  },
+  {
+    title: "Back-End Developer",
+    job: "Car Manufacture",
+    date: "2023-2024",
+    contents: [
+      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
+      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
+      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
